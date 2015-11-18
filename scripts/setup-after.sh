d62db74cce06bda816c4ea901c79e05fe1bf2da4
Andrey Novikov <novikov.andrey.01@gmail.com>
2015-11-18 19:39:10 +0300

    add phpunit

--- scripts/setup-after.sh
+++ scripts/setup-after.sh
@@ -13,6 +13,10 @@ if test -d /var/www/$HOST_NAME
 	echo "download php-cs-fixer"
 	if ! test -f "php-cs-fixer.phar"; then wget http://get.sensiolabs.org/php-cs-fixer.phar > /dev/null;fi
 
+	echo "phpunit"
+	if ! test -f "https://phar.phpunit.de/phpunit.phar"; then wget https://phar.phpunit.de/phpunit.phar > /dev/null;fi
+	if ! test -f "https://phar.phpunit.de/phpunit-old.phar"; then wget https://phar.phpunit.de/phpunit-old.phar > /dev/null;fi
+
 	mkdir -p public/temp && cd public/temp
 	echo "download adminer"
 	if ! test -f "adminer.php"; then wget -O adminer.php https://www.dropbox.com/s/d9pt98r6mj69c7s/adminer-4.2.1.php > /dev/null;fi
