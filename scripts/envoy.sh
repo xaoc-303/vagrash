4967e5ba00f9f7d8ad72df75bbd83cd51a516dd4
Andrey Novikov <novikov.andrey.01@gmail.com>
2016-02-04 23:13:48 +0300

    #envoy. update paths.

--- scripts/envoy.sh
+++ scripts/envoy.sh
@@ -6,9 +6,20 @@ echo "------------------------------"
 
 sudo su vagrant <<'EOF'
 composer global require "laravel/envoy=~1.0" --no-progress
-echo "PATH=$PATH:~/.composer/vendor/bin" >> ~/.profile
-source ~/.profile
+
+PATH_FILE="/home/vagrant/.config/composer/vendor/bin"
+if [ -h "$PATH_FILE/envoy" ]; then PATH_FIND=$PATH_FILE;fi
+
+PATH_FILE="/home/vagrant/.composer/vendor/bin"
+if [ -h "$PATH_FILE/envoy" ]; then PATH_FIND=$PATH_FILE;fi
+
+if [ ! -z $PATH_FIND ]; then
+echo "PATH=\$PATH:$PATH_FIND" >> /home/vagrant/.profile
+source /home/vagrant/.profile
 envoy
+else
+echo "The file 'envoy' does not exist"
+fi
 EOF
 
 # cd ~/.ssh
