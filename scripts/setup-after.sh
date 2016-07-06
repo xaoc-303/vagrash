dbb1f1799c69ca1b1395af889a263533aaf408ae
Andrey Novikov <novikov.andrey.01@gmail.com>
2016-07-06 04:04:42 +0300

    #fix. mysql, phpunit.

--- scripts/setup-after.sh
+++ scripts/setup-after.sh
@@ -14,8 +14,8 @@ if test -d /var/www/$HOST_NAME
 	if ! test -f "php-cs-fixer.phar"; then wget http://get.sensiolabs.org/php-cs-fixer.phar > /dev/null;fi
 
 	echo "phpunit"
-	if ! test -f "https://phar.phpunit.de/phpunit.phar"; then wget https://phar.phpunit.de/phpunit.phar > /dev/null;fi
-	if ! test -f "https://phar.phpunit.de/phpunit-old.phar"; then wget https://phar.phpunit.de/phpunit-old.phar > /dev/null;fi
+	if ! test -f "phpunit.phar"; then wget https://phar.phpunit.de/phpunit.phar > /dev/null;fi
+	if ! test -f "phpunit-old.phar"; then wget https://phar.phpunit.de/phpunit-old.phar > /dev/null;fi
 
 	mkdir -p public/temp && cd public/temp
 	echo "download adminer"
